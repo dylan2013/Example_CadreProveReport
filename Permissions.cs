b2cb4229561e106639732186f573733f52b13649
dylan2013 <dylan.huang@ischool.com.tw>
2013-10-30 10:58:41 +0800

    社團幹部證明單 - 第一版本

---
+++ Permissions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace K12.Club.CadreProveReport
+{
+    class Permissions
+    {
+        public static string 社團幹部證明單 { get { return "K12.Club.Universal.CadreProveReport.cs"; } }
+        public static bool 社團幹部證明單權限
+        {
+            get
+            {
+                return FISCA.Permission.UserAcl.Current[社團幹部證明單].Executable;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FISCA;
+using FISCA.Presentation;
+using FISCA.Permission;
+
+namespace K12.Club.CadreProveReport
+{
+    public class Program
+    {
+        [MainMethod()]
+        static public void Main()
+        {
+
+            RibbonBarItem Print = FISCA.Presentation.MotherForm.RibbonBarItems["學生", "資料統計"];
+            Print["報表"]["社團相關報表"]["社團幹部證明單"].Enable = Permissions.社團幹部證明單權限;
+            Print["報表"]["社團相關報表"]["社團幹部證明單"].Click += delegate
+            {
+                CadreProveReport cpr = new CadreProveReport();
+                cpr.ShowDialog();
+            };
+
+            Catalog detail1 = RoleAclSource.Instance["學生"]["報表"];
+            detail1.Add(new RibbonFeature(Permissions.社團幹部證明單, "社團幹部證明單"));
+        }
+    }
+}
